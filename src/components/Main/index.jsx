d440587a5174e9a160984b7796599cde195cdada
iamafif <khayotullokh00057@gmail.com>
2022-10-30 00:58:55 +0900

    change

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Btn, BtnGray, BtnOrange, Container } from './style'
+import { Btn, BtnGray, BtnOrange, BtnZero, Container } from './style'
 const Main = () => {
     const [preState, setPreState] = useState('');
     const [curState, setCurState] = useState('');
@@ -8,7 +8,14 @@ const Main = () => {
     const [total, setTotal] = useState(false);
 
     const inputNum = (e) => {
+      if (curState.includes('.') && e.target.innerText === '.') return;
 
+      if (total) setPreState('');
+
+      curState
+        ? setCurState((pre) => pre + e.target.innerText)
+        : setCurState(e.target.innerText);
+      setTotal(false)
     }
 
     const operatorType = (e) => {
@@ -33,7 +40,7 @@ const Main = () => {
   return (
     <Container>
       <Container.Wrapper>
-        <Container.Screen></Container.Screen>
+        <Container.Screen>{input}</Container.Screen>
         <BtnGray onClick={reset}>AC</BtnGray>
         <BtnGray onClick={percent}>%</BtnGray>
         <BtnGray onClick={minusPlus}>+/-</BtnGray>
@@ -50,7 +57,7 @@ const Main = () => {
         <Btn onClick={inputNum}>2</Btn>
         <Btn onClick={inputNum}>3</Btn>
         <BtnOrange onClick={operatorType}>-</BtnOrange>
-        <Btn onClick={inputNum}>0</Btn>
+        <BtnZero onClick={inputNum}>0</BtnZero>
         <Btn onClick={inputNum}>.</Btn>
         <Btn onClick={equals}>=</Btn>
       </Container.Wrapper>
--- src/components/Main/style.js
+++ src/components/Main/style.js
@@ -1,15 +1,77 @@
-import styled from 'styled-components'
-
+import styled, { css } from 'styled-components'
+  
 const Container = styled.div`
+  height: 80vh;
+  width: 50vh;
+  background-color: black;
+  border-radius: 1rem;
+  -webkit-box-shadow: 7px 7px 5px 0px rgba(50, 50, 50, 0.75);
+  -moz-box-shadow: 7px 7px 5px 0px rgba(50, 50, 50, 0.75);
+  box-shadow: 7px 7px 5px 0px rgba(50, 50, 50, 0.75); 
+  padding: 1rem;
+`;
+
+const btn = css`
+  display:grid;
+  place-content: center;
+  background-color: rgb(54,52,52);
+  border-radius: 50%;
+  color: white;
+  cursor: pointer;
+  font-weight: bold;
+
+  :hover{
+    background-color: grey;
+  }
 `
-const BtnGray = styled.div``
+const BtnGray = styled.div`
+  ${btn}
+  background-color: gray;
+  color: black;
 
-const Btn = styled.div``
+  :hover{
+    background-color: white;
+  }
+`;
 
-const BtnOrange = styled.div``
+const Btn = styled.div`
+  ${btn}
+`;
 
-Container.Wrapper = styled.div``
+const BtnZero = styled.div`
+  ${btn}
+  grid-column: 1 / span 2;
+  border-radius: 25px;
+  place-content: unset;
+  align-items: center;
+  padding-left: 2rem;
+`;
+const BtnOrange = styled.div`
+  ${btn}
+  background-color: orange;
 
-Container.Screen = styled.div``
+  :hover{
+    background-color: white;
+    color: orange;
+  }
+`;
 
-export {Container,BtnGray,Btn,BtnOrange}
+Container.Wrapper = styled.div`
+  display: grid;
+  height: 100%;
+  width: 100%;
+  grid-template-columns: repeat(4,1fr);
+  grid-template-rows: repeat(6,1fr);
+  gap:10px;
+`
+
+Container.Screen = styled.div`
+  grid-column: 1 / -1;
+  color: white;
+  text-align: right;
+  padding: 0.5rem;
+  font-size: 4rem;
+  overflow: hidden;
+`
+
+export {Container,BtnGray,Btn,BtnOrange,BtnZero}
